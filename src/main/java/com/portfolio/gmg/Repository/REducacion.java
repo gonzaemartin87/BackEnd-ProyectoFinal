9bd442ab16cda26ad77fa67ff8cf94dc579a32fd
Gonza Martín <gonzaemartin@outlook.com>
2023-03-27 12:13:33 -0300

    Componente Educación finalizado y corriendo correctamente

---
+++ src/main/java/com/portfolio/gmg/Repository/REducacion.java
@@ -0,0 +1,17 @@
+
+package com.portfolio.gmg.Repository;
+
+import com.portfolio.gmg.Entity.Educacion;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Gonzalo Martin
+ */
+@Repository
+public interface REducacion extends JpaRepository<Educacion, Integer> {
+    public Optional<Educacion> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
