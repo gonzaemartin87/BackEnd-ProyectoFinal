0993c73a13ef5e87b1851047318dc8b83d95e5b3
Gonza Martín <gonzaemartin@outlook.com>
2023-03-29 14:17:48 -0300

    make it better

--- src/main/java/com/portfolio/gmg/Controller/CHys.java
+++ src/main/java/com/portfolio/gmg/Controller/CHys.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-@RequestMapping("/hys")
+@RequestMapping("/skill")
 public class CHys {
 
     @Autowired
