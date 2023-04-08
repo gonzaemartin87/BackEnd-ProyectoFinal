dae38f8758888b41386f2c96057f1bf0da89aeea
Gonza Martín <gonzaemartin@outlook.com>
2023-04-07 21:05:24 -0300

    .

---
+++ src/main/java/com/portfolio/gmg/WebConfig.java
@@ -0,0 +1,21 @@
+package com.portfolio.gmg;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author Gonzalo Martin
+ */
+        
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**");
+    }
+    
+}
